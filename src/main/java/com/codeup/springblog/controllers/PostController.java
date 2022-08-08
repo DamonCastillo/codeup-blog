f1a79db5eb819164f4a113f1baa6e930653f5f32
Damon Castillo <damono7.castillo@gmail.com>
2022-08-08 16:14:01 -0500

    email service completed

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,19 +3,24 @@ package com.codeup.springblog.controllers;
 import com.codeup.springblog.dao.PostRepository;
 import com.codeup.springblog.dao.UserRepository;
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.services.EmailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
-
+    private final EmailService emailService;
     private PostRepository postsDao;
+    @Value("${spring.mail.from}")
     private UserRepository usersDao;
+    private String from;
 
-    public PostController(PostRepository postsDao, UserRepository usersDao) {
+    public PostController(PostRepository postsDao, UserRepository usersDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.usersDao = usersDao;
+        this.emailService = emailService;
     }
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
@@ -30,22 +35,26 @@ public class PostController {
         return "/posts/show";
     }
 
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    public String viewCreatePostForm() {
+
+    @GetMapping("/posts/create")
+    public String showCreatePostForm(Model model) {
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
-
-    @PostMapping("/posts/create")
-    public String savePost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
-        postsDao.save(new Post(title, body, usersDao.getById(1L)));
-        return "redirect:/posts";
+    @RequestMapping(path = "/post/{name}", method = RequestMethod.GET)
+    public String helloToYou(@PathVariable String name, Model model) {
+        System.out.println(from);
+        model.addAttribute("name", name);
+        emailService.prepareAndSend(usersDao.findByUsername(name), "I just emailed to say Hi!", "How are you doing?");
+        return "hello";
     }
+
     @PostMapping ("/posts/create")
     public String createPost(@ModelAttribute Post post){
        postsDao.save(post);
         return "redirect:/posts";
     }
-    @GetMapping("/posts/edit/{id}")
+    @GetMapping("/posts/{id}/edit")
     public String editPost(Model model, @PathVariable long id){
         model.addAttribute("post", postsDao.getById(id));
         return "posts/create";
